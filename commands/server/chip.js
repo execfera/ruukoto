1cc57106b2dfd9663e1d59f86e98b16e8ab1073d
execfera <amirfuhairah@gmail.com>
2017-06-18 16:30:33 +0800

    make chip/virus search case insensitive, update jsons

--- commands/server/chip.js
+++ commands/server/chip.js
@@ -1,15 +1,19 @@
 var chipData = require(__root + "/storage/chip.json");
+var reduceChip = Object.keys(chipData).reduce(function (keys, k) { 
+	keys[k.toLowerCase()] = k; 
+	return keys;
+}, {});
 
 module.exports = {
 	desc: "Returns RE:RN Battlechip data.\nUSAGE: -chip [BATTLECHIP]\nEXAMPLE: -chip Cannon",
 	lvl: "rern",
 	func (msg, cmd, bot) {
 		if (!cmd) { msg.channel.send(require('path').parse(__filename).name + ": " + this.desc, {code: true}); }
-		else if (!(cmd in chipData)) { msg.channel.send("Chip not found."); }
+		else if (!(cmd.toLowerCase() in reduceChip)) { msg.channel.send("Chip not found."); }
 		else {
 			var chipmoji = bot.guilds.get("208498945343750144").emojis.get("265842020298391552");
-			var ccontent = chipData[cmd].desc.replace(/<br>/g,'\n');
-			var ctitle = msg.guild.id === "208498945343750144" ? chipmoji + ' ' + cmd : cmd;
+			var ccontent = chipData[reduceChip[cmd.toLowerCase()]].desc.replace(/<br>/g,'\n');
+			var ctitle = msg.guild.id === "208498945343750144" ? chipmoji + ' ' + reduceChip[cmd.toLowerCase()] : reduceChip[cmd.toLowerCase()];
 			msg.channel.send(ctitle.markbold() + "\n" + ccontent.codeblock());
 		}
 	}
--- commands/server/virus.js
+++ commands/server/virus.js
@@ -1,4 +1,10 @@
 var virusData = Object.assign(require(__root + "/storage/virus1.json"), require(__root + "/storage/virus2.json"));
+var reduceVirus = {};
+for (var key in virusData) {
+	for (var i = 0; i <= 6; i++) {
+		reduceVirus[virusData[key].virus[i].name.toLowerCase()] = [key, i];
+	}
+}
 
 module.exports = {
 	desc: "Returns RE:RN virus data.\nUSAGE: -virus [VIRUSNAME]\nEXAMPLE: -virus CannonGuard2 EX",
@@ -7,18 +13,11 @@ module.exports = {
 		if (msg.channel.id === "208746890932649995" || msg.channel.id === "268383263599493122") {
 			var metool = bot.guilds.get("208498945343750144").emojis.get("278711463500316673");
 			if (!cmd) { msg.channel.send(require('path').parse(__filename).name + ": " + this.desc, {code: true}); }
-			else {
-				var found = false, foundkey = "", foundidx = 0;
-				for (var key in virusData) {
-					for (var i = 0; i <= 6; i++) {
-						if (virusData[key].virus[i].name === cmd) {
-							found = true; foundkey = key;	foundidx = i;
-							break;
-						}
-					}
-				}
-				if (found) msg.channel.send(metool + " " + cmd.markbold() + "\n" + virusData[foundkey].virus[foundidx].desc.replace(/<br>/g,'\n').codeblock());
+			else if (cmd.toLowerCase() in reduceVirus) {
+				var foundVirus = reduceVirus[cmd.toLowerCase()];
+				msg.channel.send(metool + " " + foundVirus[0].markbold() + "\n" + virusData[foundVirus[0]].virus[foundVirus[1]].desc.replace(/<br>/g,'\n').codeblock());
 			}
+			else msg.channel.send("Virus not found.");
 		}
 	}
 }
