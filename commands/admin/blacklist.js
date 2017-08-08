95aa8f8f340384f95b6fbc0bfbbd15eea24859bf
execfera <amirfuhairah@gmail.com>
2017-08-08 13:14:34 +0800

    blacklist: rejects invalid targets music: remove pause/resume

--- commands/admin/blacklist.js
+++ commands/admin/blacklist.js
@@ -6,19 +6,23 @@ module.exports = {
 	alias: ["blist"],
 	lvl: "man_msg_usr",
 	func (msg, cmd, bot) {
-		if (!cmd) { msg.channel.send(require('path').parse(__filename).name + ": " + this.desc, {code: true});  }
-		else {
+        if (!cmd) { msg.channel.send(require('path').parse(__filename).name + ": " + this.desc, {code: true});  }
+        else {
             if (msg.member.hasPermission("MANAGE_MESSAGES") || msg.author.id === "91327883208843264") {
-                if (blacklist.indexOf(cmd.mention2id()) === -1) {
-                    blacklist.push(cmd.mention2id());
-                    jsonfile.writeFileSync(__root + "/storage/blist.json", blacklist, {spaces: 2});
-                    msg.channel.send(`${cmd} added to command blacklist.`);
+                if (cmd.slice(0,2) === "<@" && cmd.slice(-1) === ">") {
+                    if (blacklist.indexOf(cmd.mention2id()) === -1) {
+                        blacklist.push(cmd.mention2id());
+                        jsonfile.writeFileSync(__root + "/storage/blist.json", blacklist, {spaces: 2});
+                        msg.channel.send(`${cmd} added to command blacklist.`);
+                    } else {
+                        blacklist.splice(blacklist.indexOf(cmd.mention2id()), 1);
+                        jsonfile.writeFileSync(__root + "/storage/blist.json", blacklist, {spaces: 2});
+                        msg.channel.send(`${cmd} removed from command blacklist.`)
+                    }
                 } else {
-                    blacklist.splice(blacklist.indexOf(cmd.mention2id()), 1);
-                    jsonfile.writeFileSync(__root + "/storage/blist.json", blacklist, {spaces: 2});
-                    msg.channel.send(`${cmd} removed from command blacklist.`)
+                    msg.channel.send("Invalid blacklist target.");
                 }
             } 
-		}
+        }
 	}
 }
