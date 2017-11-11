c40826eca84c5c842e198f68092d03d0a2099546
execfera <amirfuhairah@gmail.com>
2017-11-11 18:15:24 +0800

    logs? also message info I guess l0l

--- commands/utility/logs.js
+++ commands/utility/logs.js
@@ -1,39 +1,29 @@
 module.exports = {
-	desc: "Exports conversation logs.\nUSAGE: -logs, -logs -dm [USER_ID], -logs -text [CHANNEL_ID]. Add a limit number after the ID for message limit.",
+	desc: "Exports conversation logs.\nUSAGE: -logs, -logs -dm [USER_ID], -logs -text [CHANNEL_ID]. Add a -limit parameter for message limit (default: 100 messages). Set limit as -1 to fetch the entire channel's history.",
 	lvl: "all",
 	func (msg, cmd, bot) {
 		var location, contents = [];
 		var args = cmd.split(' ');
-		if (!cmd) location = msg.channel;
-		else if (args[0][0] === '-' && args[1]) {
-			if (args[0].slice(1) === "dm") location = bot.users.get(args[1]).dmChannel; 
-			else if (args[0].slice(1) === "text") location = bot.channels.get(args[1]);
-			var limit = args[2] ? args[2] : 100;
-		}
+		if (args.indexOf('-dm') > -1) location = bot.users.get(args[args.indexOf('-dm')+1]).dmChannel; 
+		if (args.indexOf('-text') > -1) location = bot.channels.get(args[args.indexOf('-text')+1]);
+		location = location ? location : msg.channel;
 		var locationName = location.type === "dm" ? "DM_" + location.recipient.username : "Channel_" + location.name;
-		if (limit <= 100) { 
-			location.fetchMessages({limit: limit}).then(messages => {
-				messages.forEach(function(elem) { contents.push(`[${elem.createdAt.toString().split(' ')[4]}] ${elem.author.username}: ${elem.content}`); });
-				msg.channel.send({file: {attachment: Buffer.from(contents.reverse().join('\n')), name: `Logs_${locationName}.txt`}});
-			});
-		}
-		else {
-			function recFetch (lastid, cont) { 
-				return location.fetchMessages({limit: limit <= 100 ? limit : 100, before: lastid}).then(messages => {
-					var recId;
-					messages.forEach(function(elem) { 
-						cont.push(`[${elem.createdAt.toString().split(' ')[4]}] ${elem.author.username}: ${elem.content}`);
-						recId = elem.id;
-					});
-					limit -= 100;
-					return limit > 0 ? recFetch(recId, cont) : cont;
-				})
-				.catch(e => console.error(e));
-			}
-			recFetch(undefined, contents).then(contents => {
-				msg.channel.send({file: {attachment: Buffer.from(contents.reverse().join('\n')), name: `Logs_${locationName}.txt`}});
+		var limit = args.indexOf('-limit') > -1 ? Number(args[args.indexOf('-limit')+1]) : 100;
+		if (limit === -1) limit = Number.MAX_SAFE_INTEGER;
+		function recFetch (lastid, cont) { 
+			return location.fetchMessages({limit: 100, before: lastid}).then(messages => {
+				var recId;
+				messages.forEach(function(elem) { 
+					cont.push(`[${elem.createdAt.toISOString().slice(0,-8)}] ${elem.author.username}: ${elem.content}`);
+					recId = elem.id;
+				});
+				limit -= 100;
+				return (limit <= 0 || messages.size < 100) ? cont : recFetch(recId, cont);
 			})
-			
+			.catch(e => console.error(e));
 		}
+		recFetch(undefined, contents).then(contents => {
+			msg.channel.send({file: {attachment: Buffer.from(contents.reverse().join('\n')), name: `Logs_${locationName}.txt`}});
+		});			
 	}
 }
--- commands/utility/messageinfo.js
+++ commands/utility/messageinfo.js
@@ -1,10 +1,16 @@
 module.exports = {
-	desc: "Returns message info about the requested user. If no user is provided, returns info about the requester. Use '-all' parameter to display a leaderboard of highest message counts and '-full' to display all message counts in a file.\nUSAGE: -messageinfo, -messageinfo [@USER_MENTION]\nEXAMPLE: -messageinfo @Ms. Prog#1162",
+	desc: "Returns message info about the requested user or channel. Use '-all' parameter to display a leaderboard of highest message counts and '-full' to display all message counts in a file. If no input is provided, returns info about the requester. Use -reverse to invert results.\nUSAGE: -messageinfo, -messageinfo [@USER_MENTION]\nEXAMPLE: -messageinfo @Frelia,nyu#2084",
 	lvl: "all",
 	func (msg, cmd, bot) {
 		var userid;
+		var reversed = 1;
+		if (cmd.indexOf('-reverse') > -1) {
+			cmd = cmd.split(" ").filter(e => e !== "-reverse").join('').trim();
+			reversed = -1;
+		}
 		if (!cmd) userid = msg.author.id;
 		else if (cmd === '-all' || cmd === '-full') userid = 0;
+		else if (cmd[0] === '<' && cmd[1] === '#') userid = 1;
 		else userid = cmd.mention2id();
 		var content = "";
 		(async () => {
@@ -23,7 +29,7 @@ module.exports = {
 				}
 				var compare = function(arr) {
 					return function(a, b) {
-						return ((Number(arr[a]) < Number(arr[b])) ? 1 : ((Number(arr[a]) > Number(arr[b])) ? -1 : 0));
+						return ((Number(arr[a]) < Number(arr[b])) ? reversed*1 : ((Number(arr[a]) > Number(arr[b])) ? reversed*-1 : 0));
 					};
 				};
 				utrank = utrank.sort(compare(utotals));
@@ -43,8 +49,46 @@ module.exports = {
 						content += `[${i+1}] ${rankname}: ${urates[urrank[i]]}\n`;
 					}
 				}
-				if (cmd === '-all' || content.length <= 1900) msg.channel.send(content.codeblock("css"));
+				if (content.length <= 1900) msg.channel.send(content.codeblock("css"));
 				else msg.channel.send({file: {attachment: Buffer.from(content), name: 'msgrank.txt'}});
+			}
+			else if (userid === 1) {
+				var chn = msg.guild.channels.get(cmd.slice(2,-1));
+				if (msg.guild.members.size !== msg.guild.memberCount) await msg.guild.fetchMembers();
+				var uarray = msg.guild.members.keyArray();
+				var utotals = [], urates = [], utrank = [], urrank = [];
+				for (let i = 0; i < uarray.length; i++) {
+					utrank.push(i); urrank.push(i);
+				}
+				for (let i = 0; i < msg.guild.members.size; i++) {
+					let msgsrc = await chn.search({author: uarray[i], sortOrder: "asc", limit: 1});
+					let firstmsg = msgsrc.totalResults ? msgsrc.messages[0].find(m => m.hit).createdTimestamp : 0;
+					utotals.push(msgsrc.totalResults);
+					urates.push((msgsrc.totalResults*86400000/(new Date().getTime() - firstmsg)).toFixed(2));
+				}
+				var compare = function(arr) {
+					return function(a, b) {
+						return ((Number(arr[a]) < Number(arr[b])) ? reversed*1 : ((Number(arr[a]) > Number(arr[b])) ? reversed*-1 : 0));
+					};
+				};
+				utrank = utrank.sort(compare(utotals));
+				urrank = urrank.sort(compare(urates));
+				var ranklimit = 10;
+				content += "Total Messages:\n";
+				for (let i = 0; i < ranklimit; i++) {
+					if (utotals[utrank[i]]) {
+						let rankname = msg.guild.members.get(uarray[utrank[i]]).nickname ? `${msg.guild.members.get(uarray[utrank[i]]).user.username} (${msg.guild.members.get(uarray[utrank[i]]).nickname})` : msg.guild.members.get(uarray[utrank[i]]).user.username;
+						content += `[${i+1}] ${rankname}: ${utotals[utrank[i]]}\n`;
+					}
+				}
+				content += "\nMessages Per Day:\n";
+				for (let i = 0; i < ranklimit; i++) {
+					if (urates[urrank[i]]) {
+						let rankname = msg.guild.members.get(uarray[urrank[i]]).nickname ? `${msg.guild.members.get(uarray[urrank[i]]).user.username} (${msg.guild.members.get(uarray[urrank[i]]).nickname})` : msg.guild.members.get(uarray[urrank[i]]).user.username;
+						content += `[${i+1}] ${rankname}: ${urates[urrank[i]]}\n`;
+					}
+				}
+				msg.channel.send(`Message info for ${chn}:\n${content.codeblock("css")}`);
 			}
 			else {
 				var msgsrc = await msg.guild.search({author: userid, sortOrder: "asc", limit: 1});
