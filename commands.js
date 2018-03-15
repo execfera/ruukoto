acee3baede06a0ef93187e85aab8c62e99546e01
amirfuhaira <amir@triggernext.com>
2018-03-15 12:15:01 +0800

    server-specific command no longer exist

--- commands.js
+++ commands.js
@@ -1,18 +1,13 @@
 var autoload = require('auto-load');
 
-var commands = Object.assign(autoload("./commands/author"), autoload("./commands/fun"), autoload("./commands/reference"), autoload("./commands/server"), autoload("./commands/utility"));
+var commands = Object.assign(autoload("./commands/author"), autoload("./commands/fun"), autoload("./commands/reference"), autoload("./commands/utility"));
 
 commands.help = {
 	desc: "Provides help on bot commands.\nUSAGE: -help: Lists all available commands to this user.\n-help [COMMAND]: Prints information on specific command.",
 	lvl: "all",
 	func (msg, cmd, bot) {
 		if (!cmd) {
-			var cmdlist = Object.keys(commands).filter(e => {
-				switch (commands[e].lvl) {
-					case "author": return msg.author.id === "91327883208843264";
-					default: return true;
-				}
-			}).map(e => e.code());
+			var cmdlist = Object.keys(commands).map(e => e.code());
 			msg.channel.send("Available commands: " + cmdlist.sort().join(", ") + ".\n\nUse `-help COMMAND` for more information.");
 		}
 		else msg.channel.send((cmd + ": " + commands[cmd].desc).codeblock());
